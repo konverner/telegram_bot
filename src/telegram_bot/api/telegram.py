286654b6b71cda8198b3cb38cec70e319fcc83ca
konverner <konst.verner@gmail.com>
2024-07-27 23:48:24 +0200

    add pdf and doc support

--- src/telegram_bot/api/telegram.py
+++ src/telegram_bot/api/telegram.py
@@ -1,9 +1,14 @@
 import os
+from io import BytesIO
+
 import telebot
 import logging.config
 from dotenv import load_dotenv, find_dotenv
+from fastapi import UploadFile
 from omegaconf import OmegaConf
 
+from telegram_bot.service.exceptions import UnsupportedFileTypeException
+from telegram_bot.service.file_parser import FileParser
 from telegram_bot.service.llm import FireworksLLM
 from telegram_bot.service.vector_store import VectorStore
 from telegram_bot.db.database import log_message, add_user
@@ -35,27 +40,35 @@ vector_store = VectorStore()
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Загрузите документы")
+    bot.send_message(message.chat.id, "Загрузите документы в формaте txt, docx, pdf и задавайте вопросы")
 
-## Function to load documents into ChromaDB
+
 @bot.message_handler(content_types=['document'])
 def load_document(message):
     global idx
     document = message.document
     logger.info(f"Received document: {document.file_name} with type {document.mime_type} from chat {message.from_user.username} ({message.chat.id})")
-    if document.mime_type == 'text/plain':
-        file_info = bot.get_file(document.file_id)
-        downloaded_file = bot.download_file(file_info.file_path)
-        document_text = downloaded_file.decode('utf-8')
 
+    file_info = bot.get_file(document.file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+    file_parser = FileParser(max_file_size_mb=10, allowed_file_types={"txt", "doc", "docx", "pdf"})
+
+    try:
+        upload_file = UploadFile(
+            filename=document.file_name,
+            file=BytesIO(downloaded_file),
+            size=len(downloaded_file),
+            headers={"content-type": document.mime_type}
+        )
+        document_text = file_parser.extract_content(upload_file)
         vector_store.upsert_document(document_text=document_text, idx=idx)
 
         logger.info(f"Document {document.file_name} has been upserted to ChromaDB with idx {idx}")
         idx += 1
         bot.send_message(message.chat.id, "Документ загружен.")
-    else:
+    except UnsupportedFileTypeException:
         logger.error(f"Document {document.file_name} has NOT been upserted to ChromaDB with idx {idx}")
-        bot.send_message(message.chat.id, "Пожалуйста, загрузите текстовый файл (txt).")
+        bot.send_message(message.chat.id, "Пожалуйста, загрузите текстовый файл (txt, doc, docx, pdf).")
 
 # Function to ask question about documents
 @bot.message_handler(func=lambda message: True, content_types=['text'])
